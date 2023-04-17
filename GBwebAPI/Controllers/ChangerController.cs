fb970d292a494e864836981cfd64de50eb9db8c5
Макс Сахапов <ms.mayron29@gmail.com>
2023-04-17 21:26:19 +1000

    Добавьте файлы проекта.

---
+++ GBwebAPI/Controllers/ChangerController.cs
@@ -0,0 +1,46 @@
+﻿using GBwebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GBwebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChangerController : ControllerBase
+{
+	private WetherRepository _WRepos;
+
+	public ChangerController(WetherRepository wf)
+	{
+		_WRepos = wf;
+	}
+
+	[HttpPost("Add")]
+	public IActionResult Add([FromBody] WeatherForecast wf)
+	{
+		return Ok(_WRepos.Add(wf));
+	}
+
+	[HttpDelete("Remove")]
+	public IActionResult Remove(DateOnly date)
+	{
+		return Ok(_WRepos.Remove(date));
+	}
+
+	[HttpGet("GetAll")]
+	public IEnumerable<WeatherForecast> AllInstances()
+	{
+		return _WRepos.GetAll();
+	}
+
+	[HttpGet("GetInterval")]
+	public IEnumerable<WeatherForecast> DateInterval(DateOnly start, DateOnly end)
+	{
+		return _WRepos.GetDateInterval(start, end);
+	}
+
+	[HttpPut("UpdInstance")]
+	public IActionResult Update(DateOnly date, int temp)
+	{
+		return Ok(_WRepos.Update(date, temp));
+	}
+}
---
+++ GBwebAPI/Services/WetherRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace GBwebAPI.Services;
+
+public class WetherRepository
+{
+	private List<WeatherForecast> _wf;
+
+	public WetherRepository()
+	{
+		_wf = new List<WeatherForecast>();
+	}
+
+	public string Add(WeatherForecast wf)
+	{
+		if (!_wf.Contains(wf))
+		{
+			_wf.Add(wf);
+			return "Added new instance";
+		}
+		return "Entered instance exists";
+	}
+
+	public string Remove(DateOnly date)
+	{
+		if (_wf.Find(i => i.Date == date) != null)
+		{
+			_wf.Remove(_wf.Find(i => i.Date == date)!);
+			return "Removed entred instance";
+		}
+		return "Entred instance doesn't exist";
+	}
+
+	public string Update(DateOnly entrdDate, int temp)
+	{
+		if (_wf.Find(i => i.Date == entrdDate) != null)
+		{
+			_wf.Find(i => i.Date == entrdDate)!.TemperatureC = temp;
+			return "Temperature changed";
+		}
+		return "Obj not found";
+	}
+
+	public IEnumerable<WeatherForecast> GetDateInterval(DateOnly start, DateOnly end)
+	{
+		return _wf.Where(i => i.Date >= start && i.Date <= end);
+	}
+
+	public IEnumerable<WeatherForecast> GetAll() => _wf;
+}
